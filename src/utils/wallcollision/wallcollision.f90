36b5bf3139866beff13b0e3b03b2cf94a6092091
Tiras Lin <tiraslin@stanford.edu>
2017-03-24 18:02:38 -0700

    wallcollision now outputs the interarrival times. Still need to clean up this code.

--- src/utils/wallcollision/wallcollision.f90
+++ src/utils/wallcollision/wallcollision.f90
@@ -1,21 +1,25 @@
-!Tiras Lin
+!Tiras Y. Lin
 !Program to count the number of times each bead contacts the wall
 !March 23, 2017
 
 program wallcollision
-  
+
   implicit none
-  
-  !declare variables
+
+  !variable declaration
   integer,parameter :: dp=selected_real_kind(p=15,r=307)
-  
+
   integer :: narg,iarg,nchain,nseg,nbead,ntotseg,ichain,iseg
   integer :: iskip,nskip
   integer :: nseg_bb
-  integer :: wall_dir
   integer :: itime, ntime
   integer :: totcollisions
   integer :: iwall_idx,iaway_idx
+  integer :: nbin,ibin
+  real(dp),allocatable :: dbin(:)
+  integer, allocatable :: alpha(:),beta(:)
+  real(dp),allocatable :: midbin(:,:)
+  real(dp), allocatable :: Prob_t_wall(:,:),Prob_t_away(:,:)
   character(len=20) :: arg,buffer,dmpFile
   logical :: dmpExist
   real(dp) :: b,delw
@@ -25,18 +29,15 @@ program wallcollision
   logical, allocatable,dimension(:,:,:) :: nearwall
   integer,allocatable,target :: collisions(:)
   integer,allocatable,target :: t_wall(:,:), t_away(:,:)
-  integer,allocatable ::  t_wall_curr(:,:), t_away_curr(:,:)
+  integer,allocatable ::  t_wall_curr(:,:), t_away_curr(:,:), t_curr(:,:)
   integer,allocatable :: wall_idx(:),away_idx(:)
-  
+
   !formats for saving files
 1 format(3(e14.7,1x))
 2 format(f9.4,1x,e14.7)
 3 format(i,1x,e14.7)
 4 format(i,1x,i)
 
-  !set default parameters
-  wall_dir = 2 !the wall normal is pointing in the y direction
-  
   !read in inputs
   narg=command_argument_count()
 
@@ -73,33 +74,42 @@ program wallcollision
       case ('--delw')
         call get_command_argument(iarg+1,buffer)
         read(buffer,'(f10.3)') delw
-        print '(" Distance from wall to start consider collision: ",f10.3)',delw
+        print '(" Min distance from wall (collision) ",f10.3)',delw
       case ('--nTime')
         call get_command_argument(iarg+1,buffer)
         read(buffer,'(i10)') ntime
         print '(" No. chains: ",i10)',ntime
+      case ('--nbin')
+        call get_command_argument(iarg+1,buffer)
+        read(buffer,'(i10)') nbin
+        print '(" number of bins ",i10)',nbin
       case ('--help')
         call print_help()
         stop
     end select
   end do ! iarg
-  
-  !allocate variables
-  allocate(q(3,nseg,nchain,ntime)) 
+
+  !allocate variables using the known dimensions
+  allocate(q(3,nseg,nchain,ntime))
   allocate(collisions(nseg))
   allocate(Qeex(nchain,ntime),Qeey(nchain,ntime),Qeez(nchain,ntime))
   allocate(rx(nseg,nchain,ntime),ry(nseg,nchain,ntime),rz(nseg,nchain,ntime))
   allocate(nearwall(nseg,nchain,ntime))
-  allocate(t_wall(nseg,ntime*nchain),t_away(nseg,ntime*nchain)) 
-  allocate(t_wall_curr(nseg,nchain), t_away_curr(nseg,nchain))
+  allocate(t_wall(nseg,ntime*nchain),t_away(nseg,ntime*nchain))
+  allocate(t_curr(nseg,nchain))
   allocate(wall_idx(nseg),away_idx(nseg))
-  
+  allocate(alpha(nseg),beta(nseg))
+  allocate(dbin(nseg))
+  allocate(midbin(nseg,nbin))
+  allocate(Prob_t_wall(nseg,nbin),Prob_t_away(nseg,nbin))
+
   !open the files for reading and writing
   open (unit=1,file=adjustl(dmpFile),status='old')
-  open (unit=2,file='beadwall_pdf.dat',status='unknown')
+  open (unit=2,file='bw_pdf.dat',status='unknown')
   open (unit=3,file='t_wall.dat',status='unknown')
-  
-  !start parsing the data file and store into end to end and bead vectors
+  open (unit=4,file='t_away.dat',status='unknown')
+
+  !initialize the data
   nseg_bb=nseg
   Qeex=0._dp;Qeey=0._dp;Qeez=0._dp
   rx=0._dp;ry=0._dp;rz=0._dp
@@ -107,19 +117,21 @@ program wallcollision
   nearwall = .FALSE.
   totcollisions = 0
   t_wall = 0; t_away = 0
-  
-  !do iskip=1, nskip
-  !  read(1,*)
-  !end do
-  
+  wall_idx = 0; away_idx = 0
+  t_curr = 1 !initialized to 1, because the first time step will happen regardless.
+
+  !read in files
+  ! do iskip=1, nskip
+  !   read(1,*)
+  ! end do
   do itime=1, ntime
     do ichain=1, nchain
       do iseg=1, nseg
         read(1,*) q(1:3,iseg,ichain,itime)
         if (iseg <= nseg_bb) then
-          Qeex(ichain,itime)=Qeex(ichain,itime)+q(1,iseg,ichain,itime) 
-          Qeey(ichain,itime)=Qeey(ichain,itime)+q(2,iseg,ichain,itime) 
-          Qeez(ichain,itime)=Qeez(ichain,itime)+q(3,iseg,ichain,itime) 
+          Qeex(ichain,itime)=Qeex(ichain,itime)+q(1,iseg,ichain,itime)
+          Qeey(ichain,itime)=Qeey(ichain,itime)+q(2,iseg,ichain,itime)
+          Qeez(ichain,itime)=Qeez(ichain,itime)+q(3,iseg,ichain,itime)
           rx(iseg,ichain,itime) = Qeex(ichain,itime)
           ry(iseg,ichain,itime) = Qeey(ichain,itime)
           rz(iseg,ichain,itime) = Qeez(ichain,itime)
@@ -128,111 +140,159 @@ program wallcollision
       end do !segs
     end do !chains
   end do !time
-  
-  !set the initial condition for nearwall
+
+  !set the initial condition for nearwall logicals
   itime = 1
   do ichain=1, nchain
     do iseg=1, nseg
-      
+
       !if it's now near the wall
       if (ry(iseg,ichain,itime)<=delw) then
         collisions(iseg) = collisions(iseg)+1
         nearwall(iseg,ichain,itime) = .TRUE.
+      elseif (ry(iseg,ichain,itime)>delw) then
+        nearwall(iseg,ichain,itime) = .FALSE.
       end if
-      
-    end do 
-  end do 
-  
-  
-  !process the data and count the collisions
+
+    end do
+  end do
+
+  !process the data and count the collisions & time near the wall and time away
   do itime=2, ntime
     do ichain=1, nchain
       do iseg=1, nseg
-        
+
         !if it's now near the wall, and it wasn't previously
         if (ry(iseg,ichain,itime)<=delw .AND. nearwall(iseg,ichain,itime-1)==.FALSE.) then
-          collisions(iseg) = collisions(iseg)+1
+          collisions(iseg) = collisions(iseg) + 1
           nearwall(iseg,ichain,itime) = .TRUE.
-          
-        !do nothing: it's not near the wall, and it wasn't previously
+
+          away_idx(iseg) = away_idx(iseg) + 1
+          t_away(iseg,away_idx(iseg)) = t_curr(iseg,ichain)
+          t_curr(iseg,ichain) = 1
+
+        !it's not near the wall, and it wasn't previously
         elseif (ry(iseg,ichain,itime)>delw .AND. nearwall(iseg,ichain,itime-1)==.FALSE.) then
           nearwall(iseg,ichain,itime) = .FALSE.
-        !do nothing: it's near the wall, but was already here the last time step
+          t_curr(iseg,ichain) = t_curr(iseg,ichain) + 1
+
+        !it's near the wall, but was already here the last time step
         elseif (ry(iseg,ichain,itime)<=delw .AND. nearwall(iseg,ichain,itime-1)==.TRUE.) then
           nearwall(iseg,ichain,itime) = .TRUE.
+          t_curr(iseg,ichain) = t_curr(iseg,ichain) + 1
+
         !the bead just left the wall
         elseif (ry(iseg,ichain,itime)>delw .AND. nearwall(iseg,ichain,itime-1)==.TRUE.) then
           nearwall(iseg,ichain,itime) = .FALSE.
 
+          wall_idx(iseg) = wall_idx(iseg) + 1
+          t_wall(iseg,wall_idx(iseg)) = t_curr(iseg,ichain)
+          t_curr(iseg,ichain) = 1
+
         end if
-      end do 
-    end do 
-  end do 
+      end do
+    end do
+  end do
 
   !count up the total collisions
   do iseg = 1,nseg
     totcollisions = totcollisions + collisions(iseg)
   end do
-  
-  !now, count up the time each bead spends near the wall, and the time each bead
-  !spend away from the wall. Store these in t_wall and t_away
-  t_wall_curr = 0; t_away_curr = 0
-  wall_idx = 0; away_idx = 0
-  
-  do itime=1, ntime-1
-    do ichain=1, nchain
-      do iseg=1, nseg
-      
-        if (nearwall(iseg,ichain,itime) == .TRUE.) then
-          t_wall_curr(iseg,ichain) = t_wall_curr(iseg,ichain) + 1
-          
-          if (nearwall(iseg,ichain,itime+1) == .FALSE.) then
-            wall_idx(iseg) = wall_idx(iseg) + 1
-            t_wall(iseg,wall_idx(iseg)) = t_wall_curr(iseg,ichain)
-            t_wall_curr(iseg,ichain) = 0
+
+  !note that wall_idx(iseg), is exactly the number of elements in t_wall(iseg,:). i.e. wall_idx(iseg)+1 and so on will be 0's.
+
+  !Create pdf's of away and wall times
+  do iseg = 1,nseg
+    alpha(iseg) = minval(t_wall(iseg,1:wall_idx(iseg)))
+    beta(iseg) = maxval(t_wall(iseg,1:wall_idx(iseg)))
+    dbin(iseg) = float(beta(iseg)-alpha(iseg))/nbin
+  end do
+
+  do iseg = 1,nseg
+    do ibin=1, nbin
+      midbin(iseg,ibin)=alpha(iseg)+(dbin(iseg)/2)+(ibin-1)*dbin(iseg)
+    end do
+  end do
+
+  do iseg = 1,nseg
+    do iwall_idx = 1,wall_idx(iseg)
+      do ibin = 1,nbin
+        if (ibin /= nbin) then
+          if (t_wall(iseg,iwall_idx)>=(alpha(iseg) + dbin(iseg)*(ibin-1)) .and. t_wall(iseg,iwall_idx)<(alpha(iseg) + dbin(iseg)*(ibin))) then
+            Prob_t_wall(iseg,ibin) = Prob_t_wall(iseg,ibin) + 1/(dbin(iseg)*wall_idx(iseg))
           end if
-          
-        elseif (nearwall(iseg,ichain,itime) == .FALSE.) then
-          t_away_curr(iseg,ichain) = t_away_curr(iseg,ichain) + 1
-          
-          if (nearwall(iseg,ichain,itime+1) == .TRUE.) then
-            away_idx(iseg) = away_idx(iseg) + 1
-            t_away(iseg,away_idx(iseg)) = t_away_curr(iseg,ichain)
-            t_away_curr(iseg,ichain) = 0
+        else
+          if (t_wall(iseg,iwall_idx)>=(alpha(iseg) + dbin(iseg)*(ibin-1)) .and. t_wall(iseg,iwall_idx)<=(alpha(iseg) + dbin(iseg)*(ibin))) then
+            Prob_t_wall(iseg,ibin) = Prob_t_wall(iseg,ibin) + 1/(dbin(iseg)*wall_idx(iseg))
           end if
-          
         end if
-        
-      end do 
-    end do 
-  end do 
-  
-  !note that wall_idx(iseg), is exactly the number of elements in t_wall(iseg,:)
-  
-  
-  !!!!!!!!!!!!!!!!!!!!!!!!!
-  ! writing output
-  !!!!!!!!!!!!!!!!!!!!!!!!!
-  
+      end do
+    end do
+  end do
+
+  do iseg = 1,nseg
+    alpha(iseg) = minval(t_away(iseg,1:away_idx(iseg)))
+    beta(iseg) = maxval(t_away(iseg,1:away_idx(iseg)))
+    dbin(iseg) = float(beta(iseg)-alpha(iseg))/nbin
+  end do
+
+  do iseg = 1,nseg
+    do ibin=1, nbin
+      midbin(iseg,ibin)=alpha(iseg)+(dbin(iseg)/2)+(ibin-1)*dbin(iseg)
+    end do
+  end do
+
+  do iseg = 1,nseg
+    do iaway_idx = 1,away_idx(iseg)
+      do ibin = 1,nbin
+        if (ibin /= nbin) then
+          if (t_away(iseg,iaway_idx)>=(alpha(iseg) + dbin(iseg)*(ibin-1)) .and. t_away(iseg,iaway_idx)<(alpha(iseg) + dbin(iseg)*(ibin))) then
+            Prob_t_away(iseg,ibin) = Prob_t_away(iseg,ibin) + 1/(dbin(iseg)*away_idx(iseg))
+          end if
+        else
+          if (t_away(iseg,iaway_idx)>=(alpha(iseg) + dbin(iseg)*(ibin-1)) .and. t_away(iseg,iaway_idx)<=(alpha(iseg) + dbin(iseg)*(ibin))) then
+            Prob_t_away(iseg,ibin) = Prob_t_away(iseg,ibin) + 1/(dbin(iseg)*away_idx(iseg))
+          end if
+        end if
+      end do
+    end do
+  end do
+
+  !!!!!!!!!!!!!!!!!!!!!!
+  !!!!writing output!!!!
+  !!!!!!!!!!!!!!!!!!!!!!
+
   !collision pdf
   do iseg = 1,nseg
     write(2,3) iseg, (float(collisions(iseg))/totcollisions)
   end do
 
-  !t_away
-  iseg = 40
-  do iaway_idx = 1, away_idx(iseg)
-    write(3,4) iaway_idx, t_away(iseg,iaway_idx)
+  !t_wall for end bead
+  iseg = nseg
+  do ibin = 1,nbin
+    write(3,2) midbin(iseg,ibin), Prob_t_wall(iseg,ibin)
   end do
 
+  !t_away for end bead
+  iseg = nseg
+  do ibin = 1,nbin
+    write(4,2) midbin(iseg,ibin), Prob_t_away(iseg,ibin)
+  end do
 
   !deallocate
-  deallocate(q) 
+  deallocate(q)
   deallocate(collisions)
   deallocate(Qeex,Qeey,Qeez)
   deallocate(rx,ry,rz)
   deallocate(nearwall)
-  
+  deallocate(t_wall,t_away)
+  deallocate(t_curr)
+  deallocate(wall_idx,away_idx)
+  deallocate(alpha,beta)
+  deallocate(dbin)
+  deallocate(midbin)
+  deallocate(Prob_t_wall,Prob_t_away)
+
 contains
 
   subroutine print_help()
@@ -249,5 +309,5 @@ contains
     print '(a)', ' --delw        the distance from the wall where F_ev acts'
     print '(a)', ' --nTime       number of time steps'
   end subroutine print_help
-  
+
 end program wallcollision
