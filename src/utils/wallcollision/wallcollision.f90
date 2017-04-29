ea6558df954499c947fee1a82099769f5e7f645d
Tiras Lin <tiraslin@stanford.edu>
2017-04-28 17:32:35 -0700

    wallcollision now processes ia_times and w_coll output files

--- src/utils/wallcollision/wallcollision.f90
+++ src/utils/wallcollision/wallcollision.f90
@@ -19,30 +19,30 @@ program wallcollision
   integer :: iskip,nskip
   integer :: nseg_bb
   integer :: itime, ntime
+  integer :: icoll
   integer :: totcollisions
-  integer :: iwall_idx,iaway_idx
   integer :: nbin,ibin
   real(dp),allocatable :: midbin(:,:)
-  real(dp), allocatable :: Prob_t_away(:,:)
-  character(len=20) :: arg,buffer,dmpFile_R,dmpFile_C
+  character(len=20) :: arg,buffer,dmpFile_R,dmpFile_C,dmpFile_ia,dmpFile_coll
   logical :: dmpExist
-  real(dp) :: b,delw
+  real(dp) :: b!,delw
   real(dp),allocatable :: rrel(:,:,:,:)
   real(dp),allocatable :: CoM(:,:,:)
-
   real(dp),allocatable,dimension(:,:,:) :: rx,ry,rz
   integer,allocatable,target :: collisions(:)
-  integer,allocatable,target :: t_away(:,:)
-  integer,allocatable :: t_away_curr(:,:), t_curr(:,:)
-  integer,allocatable :: away_idx(:)
+  integer,allocatable,target :: collisions_chain(:,:)
+  integer,allocatable,target :: ia_times(:,:)
+  integer,allocatable,target :: ia_times_idx(:)
+  real(dp), allocatable :: Prob_ia_times(:,:)
   real(dp),allocatable,dimension(:) :: rx_mean,ry_mean,rz_mean
+  real(dp) :: temp1,temp2
 
   !----------------------------------------
   !         formats
   !----------------------------------------
 1 format(3(e14.7,1x))
 !2 format(f9.4,1x,f16.9)
-2 format(f9.4,1x,e14.7)
+2 format(f13.3,1x,e14.7)
 3 format(i,1x,e14.7)
 4 format(i,1x,i)
 5 format(i)
@@ -75,6 +75,24 @@ program wallcollision
         else
           print '(" CoM file Name: ",a)',dmpFile_C
         end if
+      case ('--fileia')
+        call get_command_argument(iarg+1,dmpFile_ia)
+        inquire(file=dmpFile_ia,exist=dmpExist)!check if it exist
+        if(.not.dmpExist)then
+          print '(" File ",a,"not found")',dmpFile_ia
+          stop
+        else
+          print '(" ia times file Name: ",a)',dmpFile_ia
+        end if
+      case ('--filecoll')
+        call get_command_argument(iarg+1,dmpFile_coll)
+        inquire(file=dmpFile_coll,exist=dmpExist)!check if it exist
+        if(.not.dmpExist)then
+          print '(" File ",a,"not found")',dmpFile_coll
+          stop
+        else
+          print '(" wallcollisions times file Name: ",a)',dmpFile_coll
+        end if
       case ('--nCh')
         call get_command_argument(iarg+1,buffer)
         read(buffer,'(i10)') nchain
@@ -93,10 +111,10 @@ program wallcollision
         call get_command_argument(iarg+1,buffer)
         read(buffer,'(f10.3)') b
         print '(" b: ",f10.3)',b
-      case ('--delw')
-        call get_command_argument(iarg+1,buffer)
-        read(buffer,'(f10.3)') delw
-        print '(" Min distance from wall (collision) ",f10.3)',delw
+      !case ('--delw')
+      !  call get_command_argument(iarg+1,buffer)
+      !  read(buffer,'(f10.3)') delw
+      !  print '(" Min distance from wall (collision) ",f10.3)',delw
       case ('--nTime')
         call get_command_argument(iarg+1,buffer)
         read(buffer,'(i10)') ntime
@@ -117,12 +135,10 @@ program wallcollision
   allocate(rrel(3,nseg,nchain,ntime))
   allocate(CoM(3,nchain,ntime))
   allocate(collisions(nseg))
+  allocate(collisions_chain(nseg,nchain))
   allocate(rx(nseg,nchain,ntime),ry(nseg,nchain,ntime),rz(nseg,nchain,ntime))
-  allocate(t_away(nseg,ntime*nchain))
-  allocate(t_curr(nseg,nchain))
-  allocate(away_idx(nseg))
   allocate(midbin(nseg,nbin))
-  allocate(Prob_t_away(nseg,nbin))
+  allocate(Prob_ia_times(nseg,nbin))
   allocate(rx_mean(nseg),ry_mean(nseg),rz_mean(nseg))
 
   !----------------------------------------
@@ -131,10 +147,13 @@ program wallcollision
   open (unit=1,file=adjustl(dmpFile_R),status='old')
   open (unit=2,file='wallcoll_bw_pdf.dat',status='unknown')
   !open (unit=3,file='wallcoll_t_wall.dat',status='unknown')
-  open (unit=4,file='wallcoll_t_away.dat',status='unknown')
+  open (unit=4,file='wallcoll_ia_times.dat',status='unknown')
   open (unit=5,file='wallcoll_r_mean.dat',status='unknown')
   open (unit=6,file='wallcoll_endbead.dat',status='unknown')
   open (unit=7,file=adjustl(dmpFile_C),status='old')
+  open (unit=8,file='wallcoll_allbead.dat',status='unknown')
+  open (unit=9,file=adjustl(dmpFile_ia),status='old')
+  open (unit=10,file=adjustl(dmpFile_coll),status='old')
 
   !----------------------------------------
   !  data initialization & read in data
@@ -144,10 +163,7 @@ program wallcollision
   rx=0._dp;ry=0._dp;rz=0._dp
   collisions = 0
   totcollisions = 0
-  t_away = 0
-  away_idx = 0
-  t_curr = 1 !initialized to 1, because the first time step will happen regardless.
-  Prob_t_away = 0
+  Prob_ia_times = 0
   rx_mean = 0; ry_mean= 0; rz_mean = 0
 
   !read in files
@@ -160,7 +176,6 @@ program wallcollision
       read(1,*) !don't need coordinate of the tethered bead
       do iseg=1, nseg
         read(1,*) rrel(1:3,iseg,ichain,itime)
-
         rx(iseg,ichain,itime) = CoM(1,ichain,itime)+ rrel(1,iseg,ichain,itime)
         ry(iseg,ichain,itime) = CoM(2,ichain,itime)+ rrel(2,iseg,ichain,itime)
         rz(iseg,ichain,itime) = CoM(3,ichain,itime)+ rrel(3,iseg,ichain,itime)
@@ -182,66 +197,18 @@ program wallcollision
     rz_mean(iseg) = rz_mean(iseg)/(nchain*ntime)
   end do
 
-  !----------------------------------------
-  !    consider the first time step
-  !----------------------------------------
-  itime = 1
-  do ichain=1, nchain
-    do iseg=1, nseg
-
-      !if it's now near the wall
-      if (ry(iseg,ichain,itime)<=delw) then
-        collisions(iseg) = collisions(iseg)+1
-      elseif (ry(iseg,ichain,itime)>delw) then
-        !do nothing
-      end if
+  read(10,*) !number of lines to skip
+  read(10,*) !number of lines to skip
+  read(10,*) !number of lines to skip
+  do ichain=1,nchain
+    do iseg=1,nseg
+      read(10,*) temp1,temp2, collisions_chain(iseg,ichain)
     end do
   end do
 
-  !----------------------------------------
-  !  count collisions/times for times>1
-  !----------------------------------------
-  do itime=2, ntime
-    do ichain=1, nchain
-      do iseg=1, nseg
-
-        ! !if it's now near the wall, and it wasn't previously
-        ! if (ry(iseg,ichain,itime)<=delw .AND. ry(iseg,ichain,itime-1)>delw) then
-        !   collisions(iseg) = collisions(iseg) + 1
-        !
-        !   away_idx(iseg) = away_idx(iseg) + 1
-        !   t_away(iseg,away_idx(iseg)) = t_curr(iseg,ichain)
-        !   t_curr(iseg,ichain) = 1
-        !
-        ! !it's not near the wall, and it wasn't previously
-        ! elseif (ry(iseg,ichain,itime)>delw .AND. ry(iseg,ichain,itime-1)>delw) then
-        !   t_curr(iseg,ichain) = t_curr(iseg,ichain) + 1
-        !
-        ! !it's near the wall, but was already here the last time step
-        ! elseif (ry(iseg,ichain,itime)<=delw .AND. ry(iseg,ichain,itime-1)<=delw) then
-        !   collisions(iseg) = collisions(iseg) + 1
-        !   away_idx(iseg) = away_idx(iseg) + 1
-        !   t_away(iseg,away_idx(iseg)) = t_curr(iseg,ichain)
-        !   t_curr(iseg,ichain) = 1
-        !
-        ! !the bead just left the wall
-        ! elseif (ry(iseg,ichain,itime)>delw .AND. ry(iseg,ichain,itime-1)<=delw) then
-        !   t_curr(iseg,ichain) = 1
-        ! end if
-
-        !if it's now near the wall, and it wasn't previously
-        if (ry(iseg,ichain,itime)<=delw) then
-          collisions(iseg) = collisions(iseg) + 1
-          away_idx(iseg) = away_idx(iseg) + 1
-          t_away(iseg,away_idx(iseg)) = t_curr(iseg,ichain)
-          t_curr(iseg,ichain) = 1
-
-        !it's not near the wall, and it wasn't previously
-        elseif (ry(iseg,ichain,itime)>delw) then
-          t_curr(iseg,ichain) = t_curr(iseg,ichain) + 1
-        end if
-
-      end do
+  do iseg=1,nseg
+    do ichain=1,nchain
+      collisions(iseg) = collisions(iseg) + collisions_chain(iseg,ichain)
     end do
   end do
 
@@ -251,8 +218,23 @@ program wallcollision
     totcollisions = totcollisions + collisions(iseg)
   end do
 
-  print *, 'Total number of collisions is', totcollisions
-  !note that wall_idx(iseg), is exactly the number of elements in t_wall(iseg,:). i.e. wall_idx(iseg)+1 and so on will be 0's.
+  !allocate(ia_times_chain(nseg,nchain))
+  allocate(ia_times(nseg,maxval(collisions(:))))
+  allocate(ia_times_idx(nseg))
+  ia_times_idx=1
+  read(9,*) !number of lines to skip
+  read(9,*) !number of lines to skip
+  read(9,*) !number of lines to skip
+
+  do ichain=1,nchain
+    do iseg=1,nseg
+      do icoll=1,collisions_chain(iseg,ichain)
+        read(9,*) temp1, temp2, ia_times(iseg,ia_times_idx(iseg))
+        !print *, 'iseg is: ',iseg, 'ia_time is: ',ia_times(iseg,ia_times_idx(iseg))
+        ia_times_idx(iseg) = ia_times_idx(iseg) +1
+      end do
+    end do
+  end do
 
   !----------------------------------------
   !    calculate pdfs and write output
@@ -274,26 +256,38 @@ program wallcollision
     end do
   end do
 
-  !t_away for all beads
-  call pdf(t_away,away_idx,nseg,nbin,Prob_t_away,midbin)
+  !coordinates of all beads
+  do itime = 1,ntime
+    do ichain = 1,nchain
+      do iseg = 1,nseg
+        write(8,7) rx(iseg,ichain,itime), ry(iseg,ichain,itime), rz(iseg,ichain,itime)
+      end do
+    end do
+  end do
+
+  !ia_times for all beads
+  call pdf(ia_times,collisions,nseg,nbin,Prob_ia_times,midbin)
   do iseg =1, nseg
     write(4,5) iseg
     do ibin = 1,nbin
-      write(4,2) midbin(iseg,ibin), Prob_t_away(iseg,ibin)
+      write(4,2) midbin(iseg,ibin), Prob_ia_times(iseg,ibin)
     end do
   end do
 
   !----------------------------------------
   !     deallocating
   !----------------------------------------
+  deallocate(rrel)
+  deallocate(CoM)
   deallocate(collisions)
+  deallocate(collisions_chain)
   deallocate(rx,ry,rz)
-  deallocate(t_away)
-  deallocate(t_curr)
-  deallocate(away_idx)
   deallocate(midbin)
-  deallocate(Prob_t_away)
+  deallocate(Prob_ia_times)
   deallocate(rx_mean,ry_mean,rz_mean)
+  deallocate(ia_times)
+  deallocate(ia_times_idx)
+
 
 contains
 
@@ -320,9 +314,17 @@ contains
     sum = 0._dp
 
     do iseg = 1,nseg
-      alpha(iseg) = minval(times(iseg,1:times_idx(iseg)))
-      beta(iseg) = maxval(times(iseg,1:times_idx(iseg)))
-      dbin(iseg) = float(beta(iseg)-alpha(iseg))/nbin
+      if (times_idx(iseg) > 0) then
+        alpha(iseg) = minval(times(iseg,1:(times_idx(iseg))))
+        print *, 'alpha is', alpha(iseg)
+        beta(iseg) = maxval(times(iseg,1:times_idx(iseg)))
+        print *, 'beta is', beta(iseg)
+        dbin(iseg) = float(beta(iseg)-alpha(iseg))/nbin
+      else
+        alpha(iseg) = 0
+        beta(iseg) = 0
+        dbin(iseg) = 0
+      end if
     end do
 
     do iseg = 1,nseg
@@ -372,11 +374,13 @@ contains
     print '(a)', ' --help        print usage information and exit'
     print '(a)', ' --fileR        name of the file containing R coordinate'
     print '(a)', ' --fileC        name of the file containing CoM coordinate'
+    print '(a)', ' --fileia        name of the file containing IA times'
+    print '(a)', ' --filecoll        name of the file containing collisions'
     print '(a)', ' --nCh         total No. chains'
     print '(a)', ' --nSeg        No. segments in a chain'
     print '(a)', ' --nSkip       Total No. lines to be skipped'
     print '(a)', ' --b           squared maximum of segment length'
-    print '(a)', ' --delw        the distance from the wall where F_ev acts'
+    !print '(a)', ' --delw        the distance from the wall where F_ev acts'
     print '(a)', ' --nTime       number of time steps'
     print '(a)', ' --nbin        number of bins for the pdf'
   end subroutine print_help
