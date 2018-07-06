c736b0da5a3fc6960f186b7f41571be9b21a2314
Tiras Lin <tiraslin@stanford.edu>
2018-07-06 16:19:26 -0700

    torques from the springs now rotate the particle

--- src/dilute_bs/sph_sde_mod.f90
+++ src/dilute_bs/sph_sde_mod.f90
@@ -51,10 +51,12 @@ contains
     !variables used inside advance_sph_sde
     integer :: ichain_pp, offset
     real(wp),dimension(3) :: dr_sph,dphi_sph,ax_sph
-    real(wp),dimension(3) :: p_old,q_old, rf0_rel_old,rf0_rel_new
     real(wp) :: ang_sph
+    real(wp),dimension(3) :: p_old,q_old, rf0_rel_old,rf0_rel_new
+    real(wp),dimension(3) :: rf0_unit, Tseg
     real(wp),dimension(3,3) :: R
 
+    !initializing the variables
     dr_sph(1:3) = 0._wp
     dphi_sph(1:3) = 0._wp
     R(1:3,1:3) = 0._wp
@@ -62,23 +64,34 @@ contains
     q_old(1:3)  = q_sph(:,ichain)
     rf0_rel_old(1:3) = 0._wp
     rf0_rel_new(1:3) = 0._wp
+    rf0_unit = 0._wp
+    Tseg = 0._wp
+
+    !ROTATION------------------------------------------------------
     !advance the orientation of the sphere: Brownian motion
     dphi_sph(1) = dphi_sph(1) + sqrt(3._wp/8._wp)*sqrt(hstar*sqrtPI/(this%a_sph**3))*wbl_sph_or(1,jcol)
     dphi_sph(2) = dphi_sph(2) + sqrt(3._wp/8._wp)*sqrt(hstar*sqrtPI/(this%a_sph**3))*wbl_sph_or(2,jcol)
     dphi_sph(3) = dphi_sph(3) + sqrt(3._wp/8._wp)*sqrt(hstar*sqrtPI/(this%a_sph**3))*wbl_sph_or(3,jcol)
 
+    !advance the orientation of the sphere: torques
+    do ichain_pp = 1,nchain_pp
+      offset = nseg_indx3*(ichain_pp-1)
+      rf0_unit(:) = rf0(:,ichain,ichain_pp) / (sqrt(rf0(1,ichain,ichain_pp)**2 + rf0(2,ichain,ichain_pp)**2 + rf0(3,ichain,ichain_pp)**2))
+
+      Tseg(1) = rf0_unit(2)*Fseg(offset+3) - rf0_unit(3)*Fseg(offset+2)
+      Tseg(2) = rf0_unit(3)*Fseg(offset+1) - rf0_unit(1)*Fseg(offset+3)
+      Tseg(3) = rf0_unit(1)*Fseg(offset+2) - rf0_unit(2)*Fseg(offset+1)
+
+      dphi_sph(1) = dphi_sph(1) + (3._wp/16._wp)*(hstar*sqrtPI/(this%a_sph**2))*Tseg(1)*dt(iPe,idt)
+      dphi_sph(2) = dphi_sph(2) + (3._wp/16._wp)*(hstar*sqrtPI/(this%a_sph**2))*Tseg(2)*dt(iPe,idt)
+      dphi_sph(3) = dphi_sph(3) + (3._wp/16._wp)*(hstar*sqrtPI/(this%a_sph**2))*Tseg(3)*dt(iPe,idt)
+    enddo
+
+    !axis angle representation of the angular displacement
     ang_sph = sqrt(dphi_sph(1)**2 + dphi_sph(2)**2 + dphi_sph(3)**2)
     ax_sph = dphi_sph(:)/ang_sph
 
-    ! R(1,1:3) = (/1._wp,1._wp,1._wp/)
-    ! R(2,1:3) = (/1._wp,1._wp,1._wp/)
-    ! R(3,1:3) = (/1._wp,1._wp,1._wp/)
-    ! call print_matrix(R(:,:),'R before1')
-    ! R(1,1:3) = R(1,1:3) + 2._wp * (/1._wp,1._wp,1._wp/)
-    ! R(2,1:3) = R(2,1:3) + (/1._wp,1._wp,1._wp/)
-    ! R(3,1:3) = R(3,1:3) + (/1._wp,1._wp,1._wp/)
-    ! call print_matrix(R(:,:),'R before2')
-
+    !constructing the rotation matrix (Rodriguez formula)
     R(1,1:3) = (/COS(ang_sph),0._wp,0._wp/)
     R(2,1:3) = (/0._wp,COS(ang_sph),0._wp/)
     R(3,1:3) = (/0._wp,0._wp,COS(ang_sph)/)
@@ -91,22 +104,16 @@ contains
     R(2,1:3) = R(2,1:3) + (1-COS(ang_sph))*(/ax_sph(2)*ax_sph(1),ax_sph(2)*ax_sph(2),ax_sph(2)*ax_sph(3)/)
     R(3,1:3) = R(3,1:3) + (1-COS(ang_sph))*(/ax_sph(3)*ax_sph(1),ax_sph(3)*ax_sph(2),ax_sph(3)*ax_sph(3)/)
 
-    !call print_matrix(R(:,:),'R = ')
-    !call print_vector(p_sph(:,ichain),'p (before) = ')
-    !call print_vector(q_sph(:,ichain),'q (before) = ')
+    !Rotating the orientation of the particle and the tether points
     call gemv(R,p_old(:),p_sph(:,ichain),alpha=1._wp,beta=0._wp)
     call gemv(R,q_old(:),q_sph(:,ichain),alpha=1._wp,beta=0._wp)
-    !call print_vector(p_sph(:,ichain),'p (after) = ')
-    !call print_vector(q_sph(:,ichain),'q (after) = ')
     do ichain_pp = 1,nchain_pp
-      !call print_vector(rf0(:,ichain,ichain_pp) ,'rf0(:,ichain,ichain_pp) (before)  = ')
       rf0_rel_old(1:3) = rf0(:,ichain,ichain_pp) - r_sph(:,ichain)
-      !call print_vector(rf0(:,ichain,ichain_pp) ,'rf0(:,ichain,ichain_pp) (before)  = ')
       call gemv(R,rf0_rel_old(1:3),rf0_rel_new(1:3),alpha=1._wp,beta=0._wp)
       rf0(1:3,ichain,ichain_pp) = rf0_rel_new(1:3) + r_sph(1:3,ichain)
-      !call print_vector(rf0(:,ichain,ichain_pp) ,'rf0(:,ichain,ichain_pp) (after)  = ')
     end do
 
+    !TRANSLATION------------------------------------------------------
     !advance the coordinate of the sphere: tether forces
     do ichain_pp = 1,nchain_pp
       !using Fseg
@@ -121,19 +128,16 @@ contains
       ! dr_sph(2) = dr_sph(2) - (1._wp/4)*(hstar*sqrtPI/this%a_sph)*Ftet(offset+2)*dt(iPe,idt)
       ! dr_sph(3) = dr_sph(3) - (1._wp/4)*(hstar*sqrtPI/this%a_sph)*Ftet(offset+3)*dt(iPe,idt)
     end do
-    !call print_vector(dr_sph(:),'dr_sph(:) (only tether forces)')
+
     !advance the coordinate of the sphere: Brownian motion
     dr_sph(1) = dr_sph(1) + (1._wp/sqrt(2._wp))*sqrt(hstar*sqrtPI/this%a_sph)*wbl_sph(1,jcol)
     dr_sph(2) = dr_sph(2) + (1._wp/sqrt(2._wp))*sqrt(hstar*sqrtPI/this%a_sph)*wbl_sph(2,jcol)
     dr_sph(3) = dr_sph(3) + (1._wp/sqrt(2._wp))*sqrt(hstar*sqrtPI/this%a_sph)*wbl_sph(3,jcol)
-
-    !call print_vector(dr_sph(:),'dr_sph(:) (tether forces and brownian motion)')
     r_sph(:,ichain) = r_sph(:,ichain) + dr_sph(1:3)
 
     !update the tether points on the surface of the sphere
     do ichain_pp = 1,nchain_pp
       rf0(:,ichain,ichain_pp) = rf0(:,ichain,ichain_pp) + dr_sph(1:3)
-      !call print_vector(rf0(:,ichain,ichain_pp) ,'rf0(:,ichain,ichain_pp) (after translation)  = ')
     end do
 
   end subroutine advance_sph_sde
